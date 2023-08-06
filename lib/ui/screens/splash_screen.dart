d2475d0a6935e6dee08d5ea3b27baadf27d47534
Sayman Rifat <98202630+saymanrifat@users.noreply.github.com>
2023-08-06 13:06:50 +0600

    first commit

---
+++ lib/ui/screens/splash_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:intro_widget/data/models/auth_utility.dart';
+import 'package:intro_widget/ui/screens/auth/login_screen.dart';
+import 'package:intro_widget/ui/screens/bottom_nav_base_screen.dart';
+import 'package:intro_widget/ui/utils/assets_utils.dart';
+import 'package:intro_widget/ui/widgets/screen_background.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    navigateToLogin();
+  }
+
+  Future<void> navigateToLogin() async {
+    // await Future.delayed(Duration(seconds: 4));
+    // Navigator.pushAndRemoveUntil(
+    //   context,
+    //   MaterialPageRoute(builder: (context) => const LoginScreen()),
+    //       (route) => false,
+    // );
+    Future.delayed(const Duration(seconds: 3)).then((_) async {
+      final bool isLoggedIn = await AuthUtility.checkIfUserLoggedIn();
+      if (mounted) {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) =>
+          isLoggedIn
+              ? const BottomNavBaseScreen()
+              : const LoginScreen()),
+              (route) => false,
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ScreenBackground(
+        child: Center(
+          child: SvgPicture.asset(
+            AssetsUtils.logoSVG,
+            width: 90,
+            fit: BoxFit.scaleDown,
+          ),
+        ),
+      ),
+    );
+  }
+}
